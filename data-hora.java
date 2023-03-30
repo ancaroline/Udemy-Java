19ad8ba3cad4bb7e52175728b5c7415cdbf14580
Anne Caroline <carolaragao94@gmail.com>
2023-03-30 08:58:33 -0300

    data-hora

--- data-hora.java
+++ data-hora.java
@@ -147,3 +147,134 @@ public class Program {
 
 	}
 }
+
+// Formatação
+package app;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Program {
+	public static void main(String[] args) {
+
+		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+		DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;  // para localDate
+		DateTimeFormatter fmt5 = DateTimeFormatter.ISO_INSTANT;  // para instant
+
+
+		LocalDate d01 = LocalDate.parse("2023-04-02");
+		LocalDateTime d02 = LocalDateTime.parse("2023-04-02T01:30:12");
+		Instant d03 = Instant.parse("2023-04-02T01:30:12Z");
+
+		System.out.println(d01);
+		// 2023-04-02
+		System.out.println(d01.format(fmt1));
+		// 02/04/2023
+		System.out.println(fmt1.format(d01));
+		// 02/04/2023
+
+
+		System.out.println(d02);
+		// 2023-04-02T01:30:12
+		System.out.println(d02.format(fmt2));
+		// 02/04/2023 01:30
+		System.out.println(fmt2.format(d02));
+		// 02/04/2023 01:30
+		System.out.println(fmt4.format(d02));
+		// 2023-04-02T01:30:12
+
+
+		System.out.println(fmt3.format(d03));
+		// 01/04/2023 22:30
+	}
+}
+
+// Convertendo data-hora global para local
+// Data hora global, timezone -> data-hora local
+/*
+ZoneId.getAvailableZoneIds() -> irá retornar uma coleção com os nomes dos fusos horários customizados.
+*/
+package app;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.ZoneId;
+
+public class Program {
+	public static void main(String[] args) {
+
+		LocalDate d01 = LocalDate.parse("2023-04-02");
+		LocalDateTime d02 = LocalDateTime.parse("2023-04-02T01:30:12");
+		Instant d03 = Instant.parse("2023-04-02T01:30:12Z");
+
+		// para cada string s dentro da coleção, eu irei mostrar
+		for (String s : ZoneId.getAvailableZoneIds()) {
+			System.out.println(s);
+		}
+
+		// converter Instant para LocalDate
+		LocalDate r1 = LocalDate.ofInstant(d03, ZoneId.systemDefault());  // considerando o fuso horário do meu pc
+		System.out.println("r1 = " + r1);
+		// r1 = 2023-04-01
+		LocalDate r2 = LocalDate.ofInstant(d03, ZoneId.of("Portugal"));  // considerando o fuso horário de portugal
+		
+		// converter Instant para LocalDateTime
+		LocalDateTime r3 = LocalDateTime.ofInstant(d03, ZoneId.systemDefault());
+		System.out.println("r3 = " + r2);
+		// r3 = 2023-04-01T22:30:26
+
+
+		// Como pegar um dia de uma data local
+		System.out.println("d01 dia = " + d01.getDayOfMonth());
+		System.out.println("d01 mês = " + d01.getMonthValue());
+		System.out.println("d01 ano = " + d01.getYear());
+
+		// Como pegar um dia de uma data local time
+		System.out.println("d02 hora = " + d02.getHour());
+		System.out.println("d02 minuto = " + d02.getMinute());
+	}
+}
+
+// Cálculos com Data-Hora
+package app;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Duration;
+
+public class Program {
+	public static void main(String[] args) {
+
+		LocalDate d01 = LocalDate.parse("2023-04-02");
+		LocalDateTime d02 = LocalDateTime.parse("2023-04-02T01:30:12");
+		Instant d03 = Instant.parse("2023-04-02T01:30:12Z");
+
+		// semana anterior
+		LocalDate pastWeekLocalDate = d01.minusDays(7);
+		// semana posterior
+		LocalDate nextWeekLocalDate = d01.plusDays(7);
+
+
+		LocalDateTime pastWeekLocalDateTime = d02.minusDays(7);
+		LocalDateTime nextWeekLocalDateTime = d02.plusDays(7);
+
+		Instant pastWeekInstant = d03.minus(7, ChronoUnit.DAYS);
+		Instant nextWeekInstant = d03.plus(7, ChronoUnit.DAYS);
+
+		// Duração
+		Duration t1 = Duration.between(pastWeekLocalDateTime, d02);
+		System.out.println("t1 dias = " + t1.toDays());
+		// t1 dias = 7
+
+		// Não tem como calcular com LocalDate, é preciso converter em LocalDateTime
+		// Tanto o atTime quanto o atStartOfDay convertem para LocalDateTime
+		Duration t2 = Duration.between(pastWeekLocalDate.atTime(0, 0), d01.atStartOfDay());
+		System.out.println("t2 dias = " + t2.toDays());
+		// t2 dias = 7
+
+		Duration t3 = Duration.between(pastWeekInstant, d03);
+		System.out.println("t3 dias = " + t3.toDays());
+		// t3 dias = 7
+
+		// Se calcular com data maior na frente, o valor ficará negativo.
+	}
+}
