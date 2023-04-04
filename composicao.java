375e212d77424a6f7b038b039d30b946a73f3213
Anne Caroline <carolaragao94@gmail.com>
2023-04-04 10:00:57 -0300

    exercicio sobre composicao

---
+++ composicao.java
@@ -0,0 +1,9 @@
+// COMPOSIÇÃO
+/* É um tipo de associação que permite que um objeto contenha outro.
+Relação -> "tem um" ou "tem vários" dependendo do caso.
+Vantagens:
+	- organização
+	- coesão [cada objeto será responsável por uma única coisa.]
+	- flexibilidade
+	- reuso
+
---
+++ design.java
@@ -0,0 +1,6 @@
+// DESING
+/* CATEGORIAS DE CLASSES
+Em um sistema orientado a objetos, de modo geral "tudo" é objeto.
+Por questões de design tais como: organização, flexibilidade, reuso, delegação, há várias categorias de classes:
+	VIEWS (telas do sistema), CONTROLLERS (controlador entre tela e sistema), 
+	ENTITIES, SERVICES, REPOSITORIES (objetos respondáveis por acessar os dados de um BD)
