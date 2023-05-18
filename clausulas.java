e0c94a6f849ae56036c07f3f36afeef895b9f3e8
Anne Caroline <carolaragao94@gmail.com>
2023-05-18 09:45:00 -0300

    Cláusula

---
+++ clausulas.java
@@ -0,0 +1,6 @@
+/*
+Cláusula throws: propaga a exceção ao invés de tratá-la
+Cláusula throw: lança a exceção/"corta" o método
+Exception: compilador obriga a tratar ou propagar
+RuntimeException: compilador não obriga
+*/
